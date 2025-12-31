bed03799f42b69375437a7c7a4e325c8c40eff55
INES <you@example.com>
2025-12-31 18:43:56 +0100

    GREEN - adapter setTask pour la periodicite

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,9 +1,11 @@
+const Always = require('./Always');
+
 class Scheduler {
     constructor(clock = { now: () => Date.now() }) {
         if (!clock || typeof clock.now !== 'function') {
             throw new Error('clock injectee doit avoir now');
         }
-        
+
         this.clock = clock;
         this.tasks = new Map();
     }
@@ -13,16 +15,42 @@ class Scheduler {
     }
 
     // ajouter une tache
-    setTask(name, action) {
+    setTask(name, periodicity,action) {
         if (!name || typeof name !== 'string') {
             throw new Error('Le nom ne doit pas etre vide et doit etre une chaine de caractere');
+        }
+
+        // periodicity optionnel
+        if (typeof action === 'undefined' && typeof periodicity === 'function') {
+            action = periodicity;
+            periodicity = new Always();
         }
 
         if (typeof action !== 'function') {
             throw new Error("L'action doit être une fonction");
         }
 
-        this.tasks.set(name, { name, action });
+        if (!periodicity || typeof periodicity.shouldRun !== 'function') {
+            throw new Error('Invalid periodicity');
+        }
+
+        this.tasks.set(name, {
+            name,
+            periodicity,
+            action,
+            lastRun: null
+        });
+    }
+
+    update() {
+        const now = this.clock.now();
+
+        for (const task of this.tasks.values()) {
+            if (task.periodicity.shouldRun(now)) {
+                task.action();
+                task.lastRun = now;
+            }
+        }
     }
 
     // suoprimer une tache par nom
