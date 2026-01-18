6e362081e50e172243655ab5f041c4aa27106018
TeoL <teo.ladouche@hotmail.com>
2026-01-18 12:19:24 +0100

    GREEN - Added logic for EachDay periodicity

--- src/EachDay.js
+++ src/EachDay.js
@@ -1,6 +1,9 @@
 class EachDay {
     shouldRun(now) {
-
+        const date = new Date(now);
+        return date.getUTCHours() === 0 &&
+               date.getUTCMinutes() === 0 &&
+               date.getUTCSeconds() === 0;
     }
 }
 
