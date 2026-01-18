fe15a7d27c1fbd6ea16642c902e5ca73b11414d8
TeoL <teo.ladouche@hotmail.com>
2026-01-18 14:19:59 +0100

    GREEN - Added logic for EachMonth periodicity

--- src/EachMonth.js
+++ src/EachMonth.js
@@ -1,6 +1,10 @@
 class EachMonth {
     shouldRun(now) {
-
+        const date = new Date(now);
+        return date.getUTCDate() === 1 &&
+               date.getUTCHours() === 0 &&
+               date.getUTCMinutes() === 0 &&
+               date.getUTCSeconds() === 0;
     }
 }
 
