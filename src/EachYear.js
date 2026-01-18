3418a1f20dd0d2b9d224b8730e51687e911d2f39
TeoL <teo.ladouche@hotmail.com>
2026-01-18 14:25:13 +0100

    GREEN - Added logic for EachYear periodicity

--- src/EachYear.js
+++ src/EachYear.js
@@ -1,6 +1,12 @@
 class EachYear {
     shouldRun(now) {
-
+        const date = new Date(now);
+        return date.getUTCMonth() === 0 &&
+               date.getUTCDate() === 1 &&
+               date.getUTCHours() === 0 &&
+               date.getUTCMinutes() === 0 &&
+               date.getUTCSeconds() === 0;
     }
 }
+
 module.exports = EachYear;
